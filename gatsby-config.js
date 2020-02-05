da3a72212ef93360ce68c60ec01165557a7394d3
Michael Bonner <michaelwbonner@gmail.com>
2020-02-04 21:48:21 -0700

    update to sample-portfolio in supporting files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Sample Portfolio`,
+    description: `Simple portfolio using GatsbyJS`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `sample-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
