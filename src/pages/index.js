caee5e62b1267f208862ac0e780f5cc63c5c7b69
Michael Bonner <michaelwbonner@gmail.com>
2020-02-04 19:09:17 -0700

    change verbiage in main section

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,7 @@ const Card = () => {
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
           <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
+            I'm a passionate web developer in Salt Lake using JAM technologies.
           </p>
         </div>
         <div className="px-6 py-4">
