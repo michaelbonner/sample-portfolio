defa2cd64b1768039be6731b3a4910709c6f6fc0
Michael Bonner <michaelwbonner@gmail.com>
2020-02-04 19:13:20 -0700

    change verbiage on site

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const Card = () => {
         />
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">Beautiful sunset</p>
+          <p className="text-gray-700 text-base">Beautiful sunset is nice.</p>
         </div>
         <div className="px-6 py-4">
           <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
